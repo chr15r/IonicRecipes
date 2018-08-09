e26f1189b63a0d67619edf77ee1a0512d01a3268
Chris Roche <chrisroche02@hotmail.com>
2018-08-09 16:27:53 +0100

    Add/Remove/Display ingredients

--- src/pages/shopping-list/shopping-list.ts
+++ src/pages/shopping-list/shopping-list.ts
@@ -1,19 +1,37 @@
 import { Component } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { ShoppingListService } from "../../services/shopping-list.service";
+import { Ingredient } from "../../models/ingredient";
 
 @Component({
   selector: "page-shopping-list",
   templateUrl: "shopping-list.html"
 })
 export class ShoppingListPage {
+  listItems: Ingredient[];
+
   constructor(private shoppingListService: ShoppingListService) {}
 
+  ionViewWillEnter() {
+    this.loadItems();
+  }
+
   onAddItem(form: NgForm) {
-    console.log(form);
     this.shoppingListService.addItem(
       form.value.ingredientName,
       form.value.amount
     );
+    form.reset();
+    this.loadItems();
+    console.log(this.listItems);
+  }
+
+  private loadItems() {
+    this.listItems = this.shoppingListService.getItems();
+  }
+
+  onCheckItem(index: number) {
+    this.shoppingListService.removeItem(index);
+    this.loadItems();
   }
 }
