96550f5e086ab0f7889458f50808bdb9378cec20
Chris Roche <chrisroche02@hotmail.com>
2018-08-12 00:13:20 +0100

    Firebase URL refactoring

--- src/services/shopping-list.service.ts
+++ src/services/shopping-list.service.ts
@@ -7,6 +7,7 @@ import { map } from "rxjs/operators";
 @Injectable()
 export class ShoppingListService {
   private ingredients: Ingredient[] = [];
+  private fireBaseUrl: string = 'https://ionic2-recipebook-5e0a3.firebaseio.com/';
 
   constructor(private http: HttpClient, private authService: AuthService) {}
 
@@ -29,23 +30,13 @@ export class ShoppingListService {
 
   storeList(token: string) {
     const userId = this.authService.getActiveUser().uid;
-    const httpUrl =
-      "https://ionic2-recipebook-5e0a3.firebaseio.com/" +
-      userId +
-      "/shopping-list.json" +
-      "?auth=" +
-      token;
+    const httpUrl = this.fireBaseUrl + userId + "/shopping-list.json?auth=" + token;
     return this.http.put(httpUrl, this.ingredients);
   }
 
   fetchList(token: string) {
     const userId = this.authService.getActiveUser().uid;
-    const httpUrl =
-      "https://ionic2-recipebook-5e0a3.firebaseio.com/" +
-      userId +
-      "/shopping-list.json" +
-      "?auth=" +
-      token;
+    const httpUrl = this.fireBaseUrl + userId + "/shopping-list.json?auth=" + token;
     return this.http.get<Ingredient[]>(httpUrl).pipe(
       map(data => {
         this.ingredients = data;
